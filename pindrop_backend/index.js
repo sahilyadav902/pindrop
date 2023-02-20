5f38074a3118fc251bd7f55c646277d3dc3555c0
Sahil <sahilyadav902@gmail.com>
2023-02-21 02:04:33 +0530

    cors origin check

--- pindrop_backend/index.js
+++ pindrop_backend/index.js
@@ -17,6 +17,12 @@ mongoose
 })
 .catch((err) => console.log(err));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://pindrop.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/users', userRoute);
 app.use('/api/pins', pinRoute);
 
